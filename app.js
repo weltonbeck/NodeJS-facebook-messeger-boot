7469039e675761bb1308ca55383dc8a041e3d5cf
Welton Beck <weltonbeck@Weltons-iMac.local>
2016-07-22 16:34:32 -0300

    first commit

--- app.js
+++ app.js
@@ -409,29 +409,17 @@ function sendEventsMessage(recipientId) {
         payload: {
           template_type: "generic",
           elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
+            title: "rift",              
             image_url: SERVER_URL + "/assets/rift.png",
             buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
               type: "postback",
               title: "Call Postback",
               payload: "Payload for first bubble",
             }],
           }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
+            title: "touch",              
             image_url: SERVER_URL + "/assets/touch.png",
             buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
               type: "postback",
               title: "Call Postback",
               payload: "Payload for second bubble",
